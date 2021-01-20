1dd910fba32af25736d9da4b50952f6871547dcc
Pin-chih Lin <johnylin@google.com>
2021-01-20 23:50:56 +0800

    src: codec_adapter: Add process passthrough path
    If the passthrough flag is enabled, on process() the data in the input
    buffer should be directly copied to the output buffer.
    
    We should determine how and when to set the passthrough flag.
    
    Signed-off-by: Pin-chih Lin <johnylin@google.com>

--- src/include/sof/audio/codec_adapter/codec/generic.h
+++ src/include/sof/audio/codec_adapter/codec/generic.h
@@ -187,6 +187,7 @@ struct comp_data {
 	struct sof_ipc_stream_params stream_params;
 	uint32_t period_bytes; /** pipeline period bytes */
 	uint32_t deep_buff_bytes; /**< copy start threshold */
+	bool passthrough; /**< data passthrough mode */
 };
 
 /*****************************************************************************/
